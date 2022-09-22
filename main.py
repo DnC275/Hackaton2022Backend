9db5e5aff954804129eaf85e24fad05d7ff2a2ff
DnC275 <275.denis@gmail.com>
2022-09-23 01:01:57 +0300

    base server + upload test

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-from fastapi import FastAPI, File
+from fastapi import (
+    FastAPI,
+    UploadFile,
+    File,
+)
+
+
+app = FastAPI()
+
+
+@app.post("/upload")
+def upload(file: UploadFile = File(...)):
+    try:
+        contents = file.file.read()
+        with open(file.filename, 'wb') as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+
+    return {"message": f"Successfully uploaded {file.filename}"}
--- tests/test_upload.py
+++ tests/test_upload.py
@@ -3,6 +3,6 @@ import sys
 
 url = 'http://127.0.0.1:8000/upload'
 filename = sys.argv[1]
-files = [('files', open(filename, 'rb')), ('files', open('images/2.png', 'rb'))]
-resp = requests.post(url=url, files=files) 
+file = {'file': open(filename, 'rb')}
+resp = requests.post(url=url, files=file) 
 print(resp.json())
