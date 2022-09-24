070e47b1828d95f7fce0df41ef8a6db595e8e441
iliayar <iliayar3@gmail.com>
2022-09-24 14:50:30 +0300

    feat settings: env defaults

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,9 @@
 import os
+import typing
 
+def get_env(env, default: typing.Optional[str] = None):
+    return os.environ.get(env, default)
 
 VERIFICATION_COOKIE_NAME = 'token'
-MEDIA_DIR_PATH = os.environ['MEDIA_PATH']
-SECRET_FOR_JWT = os.environ['JWT_SECRET']
+MEDIA_DIR_PATH = get_env('MEDIA_PATH', './data')
+SECRET_FOR_JWT = get_env('JWT_SECRET', 'secret')
